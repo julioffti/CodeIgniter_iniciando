cfddf06b39105e1d8aa9a5781905a1a2c7742247
julio <julio_filgueiras@live.com>
2018-01-11 13:12:59 -0200

    criando nossa model e listando dados

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -4,8 +4,12 @@ class Pages extends CI_Controller
 {
     public function index()
     {
+        $this->load->model('pages_model');
+
+        $results = $this->pages_model->get();
+
         $this->load->view('template/header');
-        $this->load->view('pages/index', ['pages' =>[]]);
+        $this->load->view('pages/index', ['pages' => $results]);
         $this->load->view('template/footer');
     }
 }
