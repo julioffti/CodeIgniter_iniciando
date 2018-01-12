5b5f99b12312f1769f7652237924e78ac0947f6f
julio <julio_filgueiras@live.com>
2018-01-12 01:50:11 -0200

    inserindo dados no banco a partir de um formulário

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -2,14 +2,36 @@
 
 class Pages extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('pages_model');
+    }
+
     public function index()
     {
-        $this->load->model('pages_model');
-
         $results = $this->pages_model->get();
 
         $this->load->view('template/header');
         $this->load->view('pages/index', ['pages' => $results]);
         $this->load->view('template/footer');
     }
+
+   public function add()
+   {
+       $this->load->library('form_validation');
+
+       $this->form_validation->set_rules('title', 'Título', 'required');
+       $this->form_validation->set_rules('body', 'Conteúdo', 'required');
+
+       if ($this->form_validation->run() === false){
+           $this->load->view('template/header');
+           $this->load->view('pages/add');
+           $this->load->view('template/footer');
+       }else {
+           $data['back'] = '/pages';
+           $this->pages_model->add();
+           $this->load->view('template/success', $data);
+       }
+   }
 }
