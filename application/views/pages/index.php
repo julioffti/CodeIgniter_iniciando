674d7273e45ee5ebf3a9d71a8955b7f8fb66ffee
julio <julio_filgueiras@live.com>
2018-01-13 19:38:27 -0200

    personalizando rotas

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -14,7 +14,7 @@
         <td class="text-right">
             <a href="/pages/<?php echo $page->id ;?>" class="btn btn-xs btn-default">Ver</a>
             <a href="/pages/<?php echo $page->id ;?>/edit" class="btn btn-xs btn-info">Editar</a>
-            <form action="/pages/<?php echo $page->id ;?>/delete" style="display: inline-block">
+            <form action="/pages/<?php echo $page->id ;?>/delete" style="display: inline-block" method="post">
                 <input type="submit" value="Remover" class="btn btn-xs btn-danger">
             </form>
         </td>
