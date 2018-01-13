63bbab8acba5afc04cd83d1a4a568c9544e78747
julio <julio_filgueiras@live.com>
2018-01-13 14:55:09 -0200

    visualizando listagem de dados

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,2 +1,24 @@
-<?php
-var_dump($pages);
+<table class="table table-hover">
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>Página</th>
+        <th class="wy-text-right">ações</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($pages as $page) : ?>
+    <tr>
+        <td><?php echo $page->id ;?></td>
+        <td><?php echo $page->title ;?></td>
+        <td class="text-right">
+            <a href="/pages/<?php echo $page->id ;?>" class="btn btn-xs btn-default">Ver</a>
+            <a href="/pages/<?php echo $page->id ;?>/edit" class="btn btn-xs btn-info">Editar</a>
+            <form action="/pages/<?php echo $page->id ;?>/delete" style="display: inline-block">
+                <input type="submit" value="Remover" class="btn btn-xs btn-danger">
+            </form>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
