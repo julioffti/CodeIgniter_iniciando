54a63a61dff58870135a9237b8957058f6d86366
julio <julio_filgueiras@live.com>
2018-01-13 20:51:55 -0200

    hooks do codIgniter

---
+++ application/hooks/AuthHook.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julio
+ * Date: 13/01/2018
+ * Time: 20:41
+ */
+
+class AuthHook
+{
+ public function check()
+ {
+     echo 'AuthHook::check()';
+ }
+}
