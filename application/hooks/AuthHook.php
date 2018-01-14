48b47756b3a5d846c944b8721a0ee8ddda0236cd
julio <julio_filgueiras@live.com>
2018-01-13 23:03:59 -0200

    trabalhando com autenticação

--- application/hooks/AuthHook.php
+++ application/hooks/AuthHook.php
@@ -8,8 +8,24 @@
 
 class AuthHook
 {
+ private $controller = [
+   'auth'
+ ];
+
  public function check()
  {
-     echo 'AuthHook::check()';
+   $CI =& get_instance();
+
+   if (!isset($CI->session)){
+     $CI->load->library('session');
+   };
+   $CI->load->helper('url');
+
+   $user = $CI->session->user ?? null;
+   $controller = $CI->uri->segment(1);
+   if (!$user and !in_array($controller, $this->controller)){
+       redirect('auth/login', 'location', 302);
+       die();
+   }
  }
 }
